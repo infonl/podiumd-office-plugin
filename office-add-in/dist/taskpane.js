e24aff72a1c537d6485108d622418ea5fc8673f9
DaxRiool <dax_riool@hotmail.com>
2025-04-01 10:39:50 +0200

    fix: fixed further merge conflicts

--- office-add-in/dist/taskpane.js
+++ office-add-in/dist/taskpane.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 769cb1f (further changes)
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
 /******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
